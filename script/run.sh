b84d989f0791aa791a87b8e0425f696b23d75e25
souo <souo.ven@gmail.com>
2017-09-26 23:44:47 +0800

    update

--- script/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BASEDIR="$(cd "`dirname "$0"`"/..; pwd)"
-
-LOGFILE=${LOGFILE:-"${BASEDIR}/biplatform.log"}
-PIGFILE=${PIDFILE:-"${BASEDIR}/biplatform.pid"}
-
-nohup /bin/bash ${BASEDIR}/bin/local-main 1>>${LOGFILE} 2>&1 &
